a56f658e20db2611c18047b1fe40eb9144886d23
Divesh0312 <165398328+Divesh0312@users.noreply.github.com>
2025-04-09 19:23:23 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,7 @@
+document.querySelector('button').addEventListener('click', function () {
+    this.style.animation = 'success-glow 0.5s ease';
+    setTimeout(() => {
+      this.style.animation = '';
+    }, 500);
+  });
+  
