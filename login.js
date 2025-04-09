d32e217060300369f09a6922e1c4ab91095b08b2
Divesh0312 <165398328+Divesh0312@users.noreply.github.com>
2025-04-09 19:20:37 +0530

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('button').addEventListener('click', function () {
-    this.style.animation = 'success-glow 0.5s ease';
-    setTimeout(() => {
-      this.style.animation = '';
-    }, 500);
-  });
-  
